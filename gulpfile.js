fdcc80337d87fc686e52067007fa9a0792e4269c
BTchorzewski <tchorzewskibartosz@gmail.com>
2018-10-28 22:30:26 +0100

    making fluid layout

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,53 @@
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+let autoprefixer = require("gulp-autoprefixer");
+let sourcemaps = require("gulp-sourcemaps");
+let browserSync = require('browser-sync').create();
+let csslint = require("gulp-csslint");
 
-
-gulp.task('default', function () {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
-
+gulp.task('browserSync', function () {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+});
+gulp.task('sass', function () {
+    return gulp.src('./sass/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+        // specifica
+        browsers: ['> 5%'],
+        cascade: false
+    }))
+        .pipe(csslint())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.reload({
+        stream: true
+    }));
 });
 
- gulp.watch('./sass/**/*.scss', ['default']);
+gulp.task('html', function() {
+    return gulp.src('./index.html')
+    .pipe(browserSync.reload({
+        stream: true
+    }))
+})
+
+gulp.task("js", function(){
+    return gulp.src("./scripts/*.js")
+    .pipe(browserSync.reload({
+        stream: true
+    }))
+})
+
+gulp.task('watch', ['browserSync', 'sass', 'html', 'js'], function () {
+    gulp.watch('./sass/**/*.scss', ['sass']);
+    gulp.watch('./index.html', ['html']);
+    gulp.watch('./scripts/*.js', ['js']);
+});
+gulp.task('default', ['sass', 'watch', 'html' ]);
