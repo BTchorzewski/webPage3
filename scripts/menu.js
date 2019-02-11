5409fac3711ddf16c7451d3e52c6347e8c848682
BTchorzewski <tchorzewskibartosz@gmail.com>
2019-02-11 19:16:48 +0100

    improved navigation script

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,22 +1,26 @@
 (function(){
-    var menu = document.querySelector('.headerLogo');
-    var navigation = document.querySelector('.navigation');
-    var wrapper = document.querySelectorAll('.wrapper');
+
+    document.addEventListener('DOMContentLoaded', function(){
+        var menu = document.querySelector('.headerLogo');
+        var navigation = document.querySelector('.navigation');
+        var wrapper = document.querySelectorAll('.wrapper');
+
+        function showNavigation() {
+            navigation.classList.toggle('navigation--seen');
+        }
+
+        function hideNavigation() {
+            navigation.classList.remove('navigation--seen');
+        }
+
+        menu.addEventListener('click', showNavigation);
+        navigation.addEventListener('click', hideNavigation);
 
 
 
-
-    function showNavigation() {
-        navigation.classList.toggle('navigation--seen');
-    }
-
-    function hideNavigation() {
-        navigation.classList.remove('navigation--seen');
-    }
-
-    menu.addEventListener('click', showNavigation);
-    navigation.addEventListener('click', hideNavigation);
+    });
     
-})();
+    
+}());
 
 
